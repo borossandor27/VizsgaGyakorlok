e682b1a1ede6480e97d5aae65d832f282fc94eca
borossandor27 <boros.sanyi@gmail.com>
2025-02-15 22:06:44 +0100

    1.2.721

--- Gyumolcsok/backendGyumolcsok/index.js
+++ Gyumolcsok/backendGyumolcsok/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import { Router } from 'express';
 import gyumolcsRouter from './gyumolcs.js';
 import keszletRouter from './keszlet.js';
 const app = express();
 const port = 3000;
+app.use(cors());
 
 app.use('/gyumolcsok', gyumolcsRouter);
 app.use('/keszlet', keszletRouter);
