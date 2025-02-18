4406e48e45d2772f6c89ad18c1ca93cdb5623c26
borossandor27 <boros.sanyi@gmail.com>
2025-02-18 22:21:44 +0100

    1.2.83

--- Gyumolcsok/frontendGyumolcsok/src/pages/HomePage.jsx
+++ Gyumolcsok/frontendGyumolcsok/src/pages/HomePage.jsx
@@ -1,2 +1,8 @@
-const HomePage = ()=><h1>Home page</h1>
+const HomePage = () =>
+    <>
+        <h1>Home page</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Nulla nec dui nec eros pharetra faucibus.
+        </p>
+    </>
 export default HomePage;
