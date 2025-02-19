e3f29ee888be0e646f060a4f9f72d8449ccf5960
borossandor27 <boros.sanyi@gmail.com>
2025-02-19 20:33:57 +0100

    1.2.84

--- Gyumolcsok/frontendGyumolcsok/src/pages/HomePage.jsx
+++ Gyumolcsok/frontendGyumolcsok/src/pages/HomePage.jsx
@@ -1,8 +1,8 @@
 const HomePage = () =>
     <>
-        <h1>Home page</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Nulla nec dui nec eros pharetra faucibus.
+        <h1>Gyümölcsök</h1>
+        <p>Üdvözöljük a gyümölcsök weboldalán! Itt mindenféle gyümölcsöt megtalálhat, amit csak elképzelhet.
         </p>
+        <div></div>
     </>
 export default HomePage;
