6309fe140dbd61fdf67e51dcbecb89f9de8562f7
borossandor27 <boros.sanyi@gmail.com>
2025-03-16 08:44:02 +0100

    1.3.6

--- UserAuthSample/database/auth_db.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE auth_db;
-USE auth_db;
-
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
-);
