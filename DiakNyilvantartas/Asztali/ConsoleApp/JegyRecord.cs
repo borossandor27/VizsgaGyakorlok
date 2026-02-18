d9be09ece3b9cc1421539c77274544e58ec95f18
borossandor27 <boros.sanyi@gmail.com>
2026-02-18 19:49:31 +0100

    1.91

---
+++ DiakNyilvantartas/Asztali/ConsoleApp/JegyRecord.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public class JegyRecord
+    {
+        public string Diak_Neve { get; set; }
+        public string Osztaly { get; set; }
+        public string Tantargy { get; set; }
+        public string Tanar_Neve { get; set; }
+        public int Jegy { get; set; }
+        public DateTime Datum { get; set; }
+    }
+}
